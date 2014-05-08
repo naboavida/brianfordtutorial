2669343577232c621357e835eeea610bef1590ad
Nuno Alexandre Boavida <ASUS@ASUSPC.(none)>
2014-05-08 16:24:32 +0100

    testar widgets

--- routes/api.js
+++ routes/api.js
@@ -12,12 +12,12 @@ exports.name = function (req, res) {
 var data = {
   "posts": [
     {
-      "title": "Lorem ipsum",
-      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+      "title": "Coliformes",
+      "text": "6"
     },
     {
-      "title": "Sed egestas",
-      "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
+      "title": "Ferro",
+      "text": "334"
     }
   ]
 };
@@ -30,7 +30,7 @@ exports.posts = function (req, res) {
     posts.push({
       id: i,
       title: post.title,
-      text: post.text.substr(0, 50) + '...'
+      text: post.text.substr(0, 50)
     });
   });
   res.json({
